ed775be946a2dc60dc3d9fe3b9417bdf6f2442a5
Supriya Panigrahi <84085196+Supriya92-github@users.noreply.github.com>
2021-11-03 17:36:49 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,45 @@
+import numpy as np
+import pandas as pd
+from flask import Flask, request, render_template
+import pickle
+import os
+
+app = Flask(__name__)
+model = pickle.load(open('Pickle_file_management.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    #input_features = [float(x) for x in request.form.values()]
+    if request.method == 'POST':
+        gre = float(request.form['pregnancies'])
+        tofel = float(request.form['glucose'])
+        rating = float(request.form['bloodpressure'])
+        sop = float(request.form['skinthickness'])
+        lor = float(request.form['insulin'])
+        cgpa = float(request.form['bmi'])
+        research = float(request.form['dpf'])
+        #coa = float(request.form['age'])
+    features_value = np.array([[gre,tofel,rating,sop,lor,cgpa,research]]) #,coa]])
+    
+    # features_name = [ "GRE Score","TOEFL Score","University Rating", "SOP", "LOR ",
+    #                    "CGPA", "Research", "Chance of Admit"]
+    
+    # df = pd.DataFrame(features_value, columns=features_name)
+    output = model.predict(features_value)
+        
+    if output > 0.5:
+        prediction = "Your have good chance of selection. Chance : {}".format(output)
+    else:
+        prediction = "Your don't have good chance of selection. Chance : {}".format(output)
+        
+
+    return render_template('result.html', prediction=prediction)
+
+if __name__ == "__main__":
+    # app.run()
+    port = int(os.environ.get('PORT',5000))
+    app.run(host='0.0.0.0', port=port)
